707877857e6c8d5aaf52591bc5d28b256456863a
Miles Johnson <mileswjohnson@gmail.com>
2013-09-18 19:23:02 -0700

    Updated app Added compass

--- resources/bootstrap/routes.php
+++ resources/bootstrap/routes.php
@@ -1,20 +1,23 @@
 <?php
 /**
- * @copyright	Copyright 2010-2013, The Titon Project
- * @license		http://opensource.org/licenses/bsd-license.php
- * @link		http://titon.io
+ * @copyright   2010-2013, The Titon Project
+ * @license     http://opensource.org/licenses/bsd-license.php
+ * @link        http://titon.io
  */
 
 use Titon\Common\Registry;
 use Titon\Route\Router;
-use Titon\Route\Route;
+use Titon\G11n\Route\LocaleRoute;
 
 /** @type \Titon\Route\Router $router */
 $router = Registry::factory('Titon\Route\Router');
 
+// Enable locale resolving
+$router->on('g11n', $g11n);
+
 // Custom routes
-$router->map(new Route('/static/(path)', ['module' => 'common', 'controller' => 'static', 'action' => 'index']));
-$router->map(new Route('/static', ['module' => 'common', 'controller' => 'static', 'action' => 'index']));
+$router->map(new LocaleRoute('static.page', '/static/(path)', ['module' => 'common', 'controller' => 'static', 'action' => 'index']));
+$router->map(new LocaleRoute('static', '/static', ['module' => 'common', 'controller' => 'static', 'action' => 'index']));
 
 // Initialize
 $router->initialize();
---
+++ web/config.rb
@@ -0,0 +1,12 @@
+require 'titon-toolkit'
+
+http_path = "/"
+css_dir = "css/"
+sass_dir = "scss/"
+images_dir = "img/"
+javascripts_dir = "js/"
+
+preferred_syntax = :scss
+output_style = :compressed
+line_comments = true
+sass_options = { :unix_newlines => true }
